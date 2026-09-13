8f6849be43ab22cd7a289e827b7dd316cf123518
Gustavo <git@passos.dev>
2026-09-12 22:18:06 -0300

    Use native path spelling in the Windows package contract

--- tests/test_package_smoke.py
+++ tests/test_package_smoke.py
@@ -14,7 +14,7 @@ class PackageSmoke(unittest.TestCase):
         smoke=module()
         with patch.object(smoke.subprocess,'check_output',return_value='NodeHarbor 0.1.20 ('+'a'*40+')\n') as command:
             smoke.check_executable(Path('/package/nodeharbor'),'0.1.20','a'*40)
-        self.assertEqual(command.call_args.args[0],['/package/nodeharbor','--version'])
+        self.assertEqual(command.call_args.args[0],[str(Path('/package/nodeharbor')),'--version'])
         with patch.object(smoke.subprocess,'check_output',return_value='NodeHarbor 0.1.200 ('+'a'*40+')\n'):
             with self.assertRaises(ValueError):smoke.check_executable(Path('/package/nodeharbor'),'0.1.20','a'*40)
         with patch.object(smoke.subprocess,'check_output',return_value='NodeHarbor 0.1.20 ('+'b'*40+')\n'):
